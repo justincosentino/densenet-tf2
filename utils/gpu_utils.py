e8e127e4c0396305d7de3c5cad3f84e7159618a1
Justin Cosentino <justintcosentino@gmail.com>
2019-11-06 15:23:21 +0800

    Add gpu_utils
    Adds a common utility for configuring GPUs. Currently supports setting the list of visible devices and `set_memory_growth=True`.

---
+++ utils/gpu_utils.py
@@ -0,0 +1,22 @@
+"""Utilities used for updating device state (e.g., gpu memory)."""
+
+from typing import List
+
+import tensorflow as tf
+
+
+def set_active_devices(devices: List[str]) -> None:
+    """Sets the list of active devices and enables memory growth limits."""
+    device_ids = set([int(gpu) for gpu in devices])
+    gpu_ids = [
+        gpu_id
+        for i, gpu_id in enumerate(tf.config.experimental.list_physical_devices("GPU"))
+        if i in device_ids
+    ]
+    if gpu_ids:
+        try:
+            tf.config.experimental.set_visible_devices(gpu_ids, "GPU")
+            for gpu_id in gpu_ids:
+                tf.config.experimental.set_memory_growth(gpu_id, True)
+        except RuntimeError as e:
+            print(e)
